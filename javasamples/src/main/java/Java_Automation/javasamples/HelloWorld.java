4ec6e54d4d393021a7a80da4ad22cddd6343aeb2
Athira <Athira@LAPTOP-K348FQ4G>
2024-01-11 00:45:14 +0530

    newproject program1

---
+++ javasamples/src/main/java/Java_Automation/javasamples/HelloWorld.java
@@ -0,0 +1,10 @@
+package Java_Automation.javasamples;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
